5a49d92ead4ab9778a346218ff752a66360c549d
dmcguire80 <dmcguire80@gmail.com>
2026-01-26 16:50:35 -0500

    chore: v0.9.21 - Integrate GitHub Secrets for Production

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCHtswJkJdK7BO0sSOs8U9ZpHUlhoDPY-U',
-  authDomain: 'linear-budgeting.firebaseapp.com',
-  projectId: 'linear-budgeting',
-  storageBucket: 'linear-budgeting.firebasestorage.app',
-  messagingSenderId: '240460909695',
-  appId: '1:240460909695:web:d3f4a2693e0912dc88434a',
-  measurementId: 'G-WXJSQ0VDQC',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
